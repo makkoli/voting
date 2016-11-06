a775097e319154d24b3bc889a5e864a9ab9b0594
makkoli <makkoli@vt.edu>
2016-11-06 03:54:50 -0500

    added chart functionality

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express'),
     MongoClient = require('mongodb').MongoClient,
     votingHelper = require('./votinghelper.js'),
+    bodyParser = require('body-parser'),
     app = express();
 
 var oneDay = 86400000;
 
 // static files
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+// parse body for posts
+app.use(bodyParser.urlencoded({ extended: true }));
 // views
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
@@ -40,8 +43,21 @@ MongoClient.connect('mongodb://localhost:27017/voting', function(err, db) {
                 console.log(err);
             }
 
-            res.render("poll", { name: docs[0].name, options: docs[0].options });
+            var chartLabels = docs[0].options.map(function(curr) { return curr.name });
+            var chartVotes = docs[0].options.map(function(curr) { return curr.votes });
+
+            res.render("poll", { name: docs[0].name, labels: chartLabels,
+                                votes: chartVotes, link: req.params.link });
         })
+    });
+
+    app.post('/poll/:link', function(req, res) {
+        var query = { link: req.params.link, "options.name": req.body.option };
+        var updateParam = { "$inc": { "options.$.votes": 1 } };
+        console.log(req.body.option);
+
+        db.collection('polls').updateOne(query, updateParam);
+        res.end();
     });
 });
 
